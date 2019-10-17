e53ec8c6b8917afc218bc5d166edd646deae70ad
lguiller <lguiller@student.42.fr>
2019-10-17 14:38:39 +0200

    ajout du l'affichage de la courbe reel en parallele de la courbe calcule

--- main.py
+++ main.py
@@ -15,19 +15,31 @@ def learningFunction(data):
     for row in data:
         for elem in row: assert (type(row[0]) == float)
     for _ in range(0, 10000):
-        tmpTheta0 = float(0)
-        tmpTheta1 = float(0)
+        tmpTheta0 = 0.0
+        tmpTheta1 = 0.0
 
         for row in data:
-            tmpTheta0 = tmpTheta0 + (estimatePrice(row[0]) - row[1])
-            tmpTheta1 = tmpTheta1 + ((estimatePrice(row[0]) - row[1]) * row[0])
-        theta0 = theta0 - (learningRate * (1.0 / float(len(data))) * tmpTheta0)
-        theta1 = theta1 - (learningRate * (1.0 / float(len(data))) * tmpTheta1)
+            tmpTheta0 += (estimatePrice(row[0]) - row[1])
+            tmpTheta1 += ((estimatePrice(row[0]) - row[1]) * row[0])
+        theta0 -= ((learningRate * tmpTheta0) / float(len(data)))
+        theta1 -= ((learningRate * tmpTheta1) / float(len(data)))
+
+def aim(data):
+    data_X = [row[0] for row in data]
+    data_Y = [row[1] for row in data]
+    sum_X = sum(data_X)
+    sum_Y = sum(data_Y)
+    mean_X = mean(data_X)
+    mean_Y = mean(data_Y)
+    Sum_of_Square_X = sum([((elem - mean_X) ** 2) for elem in data_X])
+    Sum_of_Products = sum([((row[0] - mean_X) * (row[1] - mean_Y)) for row in data])
+    aim_theta1 = Sum_of_Products / Sum_of_Square_X
+    aim_theta0 = mean_Y - aim_theta1 * mean_X
+    return (aim_theta0, aim_theta1)
 
 def main():
     global theta0
     global theta1
-    data = []
     size = (0.0, 1.0)
 
     try:
@@ -36,16 +48,17 @@ def main():
         print('usage: ./main.py [data.csv]')
         sys.exit(1)
     data, title = load_file(sys.argv[1])
+    aim_theta0, aim_theta1 = aim(data)
     minmax = dataset_minmax(data)
     normalize_data_set(normalize_elem, data, minmax, size)
     learningFunction(data)
     theta0 = rev_normalize_elem(theta0, minmax, size)
     print(theta0, theta1)
     normalize_data_set(rev_normalize_elem, data, minmax, size)
-    printGraph([row[0] for row in data], [row[1] for row in data], title, theta0, theta1)
+    printGraph([row[0] for row in data], [row[1] for row in data], title, theta0, theta1, aim_theta0, aim_theta1)
 
-theta0 = float(0)
-theta1 = float(0)
+theta0 = 0.0
+theta1 = 0.0
 
 if __name__ == '__main__':
     main()
--- tools.py
+++ tools.py
@@ -3,14 +3,16 @@
 from csv import reader
 from pylab import *
 
-def printGraph(km, price, title, theta0, theta1):
+def printGraph(x, y, title, theta0, theta1, aim_theta0, aim_theta1):
     assert (type(title[0]) == str and type(title[1]) == str)
-    scatter(km, price)
+    scatter(x, y)
     xlabel(title[0])
     ylabel(title[1])
-    lineX = [min(km), max(km)]
+    lineX = [min(x), max(x)]
     lineY = [theta1 * float(i) + theta0 for i in lineX]
+    aim_lineY = [aim_theta1 * float(i) + aim_theta0 for i in lineX]
     plot(lineX, lineY)
+    plot(lineX, aim_lineY)
     show()
 
 def dataset_minmax(data):
@@ -22,13 +24,15 @@ def dataset_minmax(data):
 def normalize_elem(elem, minmax, size):
     assert (type(elem) == float)
     for row in minmax: assert (type(row) == float)
-    return ((size[1] - size[0]) * ((elem - minmax[0]) / (minmax[1] - minmax[0])) + size[0])
+    return ((size[1] - size[0]) * ((elem - minmax[0]) /
+            (minmax[1] - minmax[0])) + size[0])
     #return ((elem - minmax[0]) / (minmax[1] - minmax[0]))
 
 def rev_normalize_elem(elem, minmax, size):
     assert (type(elem) == float)
     for row in minmax: assert (type(row) == float)
-    return (((elem - size[0]) * (minmax[1] - minmax[0]) + (size[1] - size[0]) * minmax[0]) / (size[1] - size[0]))
+    return (((elem - size[0]) * (minmax[1] - minmax[0]) +
+             (size[1] - size[0]) * minmax[0]) / (size[1] - size[0]))
     #return (elem * (minmax[1] - minmax[0]) + minmax[0])
 
 def normalize_data_set(norm_funct, data, minmax, size):
